0b9f5e16c94535984912078d553e08a0ffd59861
Blinkx-Original <martinpino@outlook.com>
2025-08-28 15:17:41 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';        // 👈 importa desde el paquete raíz
+import vercel from '@astrojs/vercel';        // 👈 paquete raíz
 import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),                         // 👈 usa el adapter
+  adapter: vercel(),
   integrations: [keystatic({})]
 });
